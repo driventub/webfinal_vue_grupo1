48358313e65a53689854a5239236a33f485f20a7
Luis Ortiz <luchoaam97@gmail.com>
2023-09-04 13:16:59 -0500

    cambios luis lunes 5 13:16

--- src/components/modules/acercamiento/pages/GuardarSuscripcion.vue
+++ src/components/modules/acercamiento/pages/GuardarSuscripcion.vue
@@ -1,21 +1,15 @@
 <template>
   <DatosGuardarSuscripcion />
-  <div class="footer">
-    <p>
-      <router-link to="/terminos">Términos y Condiciones</router-link> |
-      <router-link to="/ayuda">Ayuda</router-link> |
-      &copy; {{ new Date().getFullYear() }} Derechos Reservados
-    </p>
-  </div>
+  <FooterSuscripcion/>
 </template>
 
 <script>
 import DatosGuardarSuscripcion from '../components/DatosGuardarSuscripcion.vue';
-import Terminos from './Terminos.vue';
+import FooterSuscripcion from '../components/FooterSuscripcion.vue';
 export default {
   components: {
     DatosGuardarSuscripcion,
-    Terminos
+    FooterSuscripcion
   },
 }
 </script>
